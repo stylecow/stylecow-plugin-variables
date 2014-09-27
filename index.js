f41a504d29a9d49095416f9dfc463a262298744b
oscarotero <oom@oscarotero.com>
2014-09-27 11:23:11 +0200

    updated

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-module.exports = {
+module.exports = function (stylecow) {
 
-	//Use var() function
-	"Function": {
-		var: function (fn) {
-			var arguments = fn.content;
-			var value = fn.ancestor('Rule').getData(arguments[0]) || arguments[1];
+	stylecow.addTask({
+		//Use var() function
+		"Function": {
+			var: function (fn) {
+				var arguments = fn.content;
+				var value = fn.ancestor('Rule').getData(arguments[0]) || arguments[1];
 
-			if (value) {
-				fn.replaceWith(value);
+				if (value) {
+					fn.replaceWith(value);
+				}
+			}
+		},
+
+		//Save new --variables
+		Declaration: function (declaration) {
+			if (declaration.name.indexOf('--') === 0) {
+				var rule = declaration.ancestor('Rule');
+
+				if (rule.hasChild('Selector', [':root', 'html'])) {
+					rule.ancestor('Root').setData(declaration.name, declaration.value);
+				} else {
+					rule.setData(declaration.name, declaration.value);
+				}
+
+				declaration.remove();
 			}
 		}
-	},
-
-	//Save new --variables
-	Declaration: function (declaration) {
-		if (declaration.name.indexOf('--') === 0) {
-			var rule = declaration.ancestor('Rule');
-
-			if (rule.hasChild('Selector', [':root', 'html'])) {
-				rule.ancestor('Root').setData(declaration.name, declaration.value);
-			} else {
-				rule.setData(declaration.name, declaration.value);
-			}
-
-			declaration.remove();
-		}
-	}
+	});
 };
