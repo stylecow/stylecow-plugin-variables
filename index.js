0b70bc5004caccbc0d16f7c4063f66d857f05e6a
oscarotero <oom@oscarotero.com>
2014-12-20 20:18:53 +0100

    changes for v4 version

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ module.exports = function (stylecow) {
 		//Use var() function
 		"Function": {
 			var: function (fn) {
-				var arguments = fn.getContent();
-				var value = fn.parent({type: 'Rule'}).getData(arguments[0]);
+				var value = fn.parent({type: 'Rule'}).getData(fn[0].toString());
 
 				if (value) {
-					if (fn.parent().is({type: ['Value', 'Argument']}) && (value.length > 1)) {
-						return fn.parent().setContent(value.join(','));
-					}
+					var parent = fn.parent();
+					var index = fn.index();
+					fn.remove();
 
-					return fn.replaceWith(value.join(' '));
+					value.forEach(function (v, i) {
+						parent.splice(index + i, 0, v.clone());
+					});
 				}
 
-				if (arguments[1]) {
-					fn.replaceWith(arguments[1]);
+				if (fn[1]) {
+					fn.replaceWith(fn[1]);
 				}
 			}
 		},
@@ -26,14 +27,13 @@ module.exports = function (stylecow) {
 		Declaration: function (declaration) {
 			if (declaration.name.indexOf('--') === 0) {
 				var rule = declaration.parent({type: 'Rule'});
+				var value = declaration.detach();
 
-				if (rule.hasChild({type: 'Selector', string: [':root', 'html']})) {
-					rule.parent({type: 'Root'}).setData(declaration.name, declaration.getContent());
+				if (rule.firstChild({type: 'Selectors'}).hasChild({type: 'Selector', string: [':root', 'html']})) {
+					rule.parent({type: 'Root'}).setData(declaration.name, value);
 				} else {
-					rule.setData(declaration.name, declaration.getContent());
+					rule.setData(declaration.name, value);
 				}
-
-				declaration.remove();
 			}
 		}
 	});
