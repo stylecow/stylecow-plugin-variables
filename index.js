753d60528c4b380f66525a6b7101235a0e7e097b
oscarotero <oom@oscarotero.com>
2014-12-22 00:07:40 +0100

    api changes

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 module.exports = function (stylecow) {
 
+	//Use var() function
 	stylecow.addTask({
-
-		//Use var() function
-		"Function": {
-			var: function (fn) {
-				var value = fn.parent({type: 'Rule'}).getData(fn[0].toString());
-
-				if (value) {
-					var parent = fn.parent();
-					var index = fn.index();
-					fn.remove();
-
-					value.forEach(function (v, i) {
-						parent.splice(index + i, 0, v.clone());
-					});
-				}
-
-				if (fn[1]) {
-					fn.replaceWith(fn[1]);
-				}
-			}
+		filter: {
+			type: 'Function',
+			name: 'var'
 		},
+		fn: function (fn) {
+			var value = fn.parent({type: 'Rule'}).getData(fn[0].toString());
 
-		//Save new --variables
-		Declaration: function (declaration) {
+			if (value) {
+				var parent = fn.parent();
+				var index = fn.index();
+				fn.remove();
+
+				value.forEach(function (v, i) {
+					parent.splice(index + i, 0, v.clone());
+				});
+			}
+
+			if (fn[1]) {
+				fn.replaceWith(fn[1]);
+			}
+		}
+	});
+
+
+	//Save new --variables
+	stylecow.addTask({
+		filter: {
+			type: 'Declaration'
+		},
+		fn: function (declaration) {
 			if (declaration.name.indexOf('--') === 0) {
 				var rule = declaration.parent({type: 'Rule'});
 				var value = declaration.detach();
