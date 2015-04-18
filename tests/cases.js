4c48ac7a89e213e0732c654992dc41e1049a74e7
oscarotero <oom@oscarotero.com>
2015-04-18 19:45:46 +0200

    tests

--- tests/cases.js
+++ tests/cases.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 var stylecow = require('stylecow');
 
 stylecow
@@ -11,14 +12,18 @@ stylecow
 		"android": 0,
 		"ios": 0
 	})
-	.testCases(__dirname + '/cases', function (name, css, assert) {
-		stylecow.run(css);
+	.testCases(__dirname + '/cases', function (test) {
+		stylecow.run(test.css);
 
-		describe('cases/' + name, function() {
-			it('should match output.css and ast.json', function() {
-				assert();
+		describe('cases/' + test.name, function() {
+			it('should match output.css', function() {
+				//test.write('output.css', test.css.toString());
+				assert.equal(test.css.toString(), test.read('output.css'));
+			});
+
+			it('should match ast.json', function() {
+				//test.writeJson('ast.json', test.css.toAst());
+				assert.deepEqual(test.css.toAst(), test.readJson('ast.json'));
 			});
 		});
-
-		//assert.regenerate();
 	});
