e00c82454565d9398e7872b7ba42cee54b6491dd
oscarotero <oom@oscarotero.com>
2015-09-10 02:45:13 +0200

    code style

--- index.js
+++ index.js
@@ -1,70 +1,68 @@
 "use strict";
 
-module.exports = function (stylecow) {
+module.exports = function (tasks) {
 
-	//Use var() function
-	stylecow.addTask({
-		filter: {
-			type: 'Function',
-			name: 'var'
-		},
-		fn: function (fn) {
-			var name = fn.get('ExtensionName');
+    //Use var() function
+    tasks.addTask({
+        filter: {
+            type: 'Function',
+            name: 'var'
+        },
+        fn: function (fn) {
+            var name = fn.get('ExtensionName');
 
-			if (name) {
-				let value = fn.getData('@var-' + name.name);
+            if (name) {
+                let value = fn.getData('@var-' + name.name);
 
-				if (value) {
-					replace(fn, value.clone());
-				} else if (fn[1]) {
-					replace(fn, fn[1].clone());
-				}
-			}
-		}
-	});
+                if (value) {
+                    replace(fn, value.clone());
+                } else if (fn[1]) {
+                    replace(fn, fn[1].clone());
+                }
+            }
+        }
+    });
 
 
-	//Save new --variables
-	stylecow.addTask({
-		filter: {
-			type: 'Declaration'
-		},
-		fn: function (declaration) {
-			if (declaration.name.indexOf('--') === 0) {
-				let rule = declaration.getAncestor('Rule');
-				let name = '@var-' + declaration.name.substr(2);
+    //Save new --variables
+    tasks.addTask({
+        filter: 'Declaration',
+        fn: function (declaration) {
+            if (declaration.name.indexOf('--') === 0) {
+                let rule = declaration.getAncestor('Rule');
+                let name = '@var-' + declaration.name.substr(2);
 
-				if (
-					rule
-					.getChild('Selectors')
-					.hasChild({
-						type: 'Selector',
-						string: [':root', 'html']
-					})
-				) {
-					rule.getAncestor('Root').setData(name, declaration.detach());
-				} else {
-					rule.setData(name, declaration.detach());
-				}
-			}
-		}
-	});
+                if (
+                    rule
+                    .getChild('Selectors')
+                    .hasChild({
+                        type: 'Selector',
+                        string: [':root', 'html']
+                    })
+                ) {
+                    rule.getAncestor('Root').setData(name, declaration.detach());
+                } else {
+                    rule.setData(name, declaration.detach());
+                }
+            }
+        }
+    });
 
-	function replace (fn, values) {
-		var parent = fn.getParent();
+    function replace (fn, values) {
+        var parent = fn.getParent();
 
-		if (values.type === 'Declaration' && parent && parent.type === 'Value' && parent.length === 1) {
-			while (values.length) {
-				parent.after(values.pop());
-			}
+        if (values.type === 'Declaration' && parent && parent.type === 'Value' && parent.length === 1) {
+            while (values.length) {
+                parent.after(values.pop());
+            }
 
-			parent.detach();
-		} else {
-			while (values.length) {
-				fn.after(values.pop());
-			}
+            parent.detach();
+        } else {
+            while (values.length) {
+                fn.after(values.pop());
+            }
 
-			fn.detach();
-		}
-	}
+            fn.detach();
+        }
+    }
 };
